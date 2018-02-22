734f7263d92387b801364d6f8e28d1136ec578fc
William <nettee.liu@gmail.com>
2018-02-23 03:04:15 +0800

    add toolbar

--- app/src/main/java/me/nettee/financial/MainActivity.java
+++ app/src/main/java/me/nettee/financial/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
@@ -15,13 +16,18 @@ import java.util.Map;
 
 public class MainActivity extends Activity {
 
+    private Map<Integer, Integer> mFragmentTitleMap = new HashMap<>();
     private Map<Integer, Fragment> mFragmentMap = new HashMap<>();
     private Fragment mCurrentFragment;
+
+    private TextView mToolbarTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbarTitle = (TextView) findViewById(R.id.toolbar_title);
 
         final BottomNavigationViewEx bnve = (BottomNavigationViewEx) findViewById(R.id.bottom_navigation);
         bnve.enableAnimation(false);
@@ -42,11 +48,18 @@ public class MainActivity extends Activity {
 
     private void initFragments() {
 
+        mFragmentTitleMap.put(R.id.menu_asset, R.string.nav_asset);
+        mFragmentTitleMap.put(R.id.menu_bill, R.string.nav_bill);
+        mFragmentTitleMap.put(R.id.menu_statement, R.string.nav_statement);
+        mFragmentTitleMap.put(R.id.menu_me, R.string.nav_me);
+
         mCurrentFragment = new AssetFragment();
         mFragmentMap.put(R.id.menu_asset, mCurrentFragment);
         mFragmentMap.put(R.id.menu_bill, new BillFragment());
         mFragmentMap.put(R.id.menu_statement, new StatementFragment());
         mFragmentMap.put(R.id.menu_me, new MeFragment());
+
+        mToolbarTitle.setText(R.string.nav_asset);
 
         getFragmentManager().beginTransaction()
                 .add(R.id.fragment_container, mCurrentFragment)
@@ -62,6 +75,9 @@ public class MainActivity extends Activity {
         if (fragment == mCurrentFragment) {
             return;
         }
+
+        int stringId = mFragmentTitleMap.get(itemId);
+        mToolbarTitle.setText(stringId);
 
         getFragmentManager().beginTransaction()
                 .replace(R.id.fragment_container, fragment)
