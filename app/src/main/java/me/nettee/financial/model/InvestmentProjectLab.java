66c8807ef09acbe71a054d233929a270e58dc899
William <nettee.liu@gmail.com>
2018-03-06 22:24:04 +0800

    add investment project data

--- app/src/main/java/me/nettee/financial/model/InvestmentProjectLab.java
+++ app/src/main/java/me/nettee/financial/model/InvestmentProjectLab.java
@@ -25,27 +25,22 @@ public class InvestmentProjectLab {
         monetaryFundInvestmentProject1.setAnnualYield(Percent.valueOf(4.5521));
         monetaryFundInvestmentProject1.setBuyDate(new LocalDate("2018-02-01"));
         mInvestmentProjects.add(monetaryFundInvestmentProject1);
-        MonetaryFundInvestmentProject monetaryFundInvestmentProject2 = new MonetaryFundInvestmentProject();
-        monetaryFundInvestmentProject2.setInvestmentPlatformType(InvestmentPlatform.LUFAX);
-        monetaryFundInvestmentProject2.setName("超值基金2");
-        monetaryFundInvestmentProject2.setPrinciple(Amount.integer(20000));
-        monetaryFundInvestmentProject2.setAnnualYield(Percent.valueOf(4.5522));
-        monetaryFundInvestmentProject2.setBuyDate(new LocalDate("2018-02-02"));
-        mInvestmentProjects.add(monetaryFundInvestmentProject2);
-        MonetaryFundInvestmentProject monetaryFundInvestmentProject3 = new MonetaryFundInvestmentProject();
-        monetaryFundInvestmentProject3.setInvestmentPlatformType(InvestmentPlatform.TIANTIAN_FUND);
-        monetaryFundInvestmentProject3.setName("超值基金3");
-        monetaryFundInvestmentProject3.setPrinciple(Amount.integer(30000));
-        monetaryFundInvestmentProject3.setAnnualYield(Percent.valueOf(4.5523));
-        monetaryFundInvestmentProject3.setBuyDate(new LocalDate("2018-02-03"));
-        mInvestmentProjects.add(monetaryFundInvestmentProject3);
-        MonetaryFundInvestmentProject monetaryFundInvestmentProject4 = new MonetaryFundInvestmentProject();
-        monetaryFundInvestmentProject4.setInvestmentPlatformType(InvestmentPlatform.ANT_FORTUNE);
-        monetaryFundInvestmentProject4.setName("超值基金4");
-        monetaryFundInvestmentProject4.setPrinciple(Amount.integer(40000));
-        monetaryFundInvestmentProject4.setAnnualYield(Percent.valueOf(4.5524));
-        monetaryFundInvestmentProject4.setBuyDate(new LocalDate("2018-02-04"));
-        mInvestmentProjects.add(monetaryFundInvestmentProject4);
+        MonetaryFundInvestmentProject ljb = new MonetaryFundInvestmentProject();
+        ljb.setInvestmentPlatformType(InvestmentPlatform.LUFAX);
+        ljb.setName("陆金宝T+1");
+        ljb.setPrinciple(Amount.integer(1000));
+        ljb.setAnnualYield(Percent.valueOf(4.38));
+        ljb.setBuyDate(new LocalDate("2018-02-10"));
+        ljb.setValueDate(new LocalDate("2018-02-13"));
+        mInvestmentProjects.add(ljb);
+        MonetaryFundInvestmentProject nfttl = new MonetaryFundInvestmentProject();
+        nfttl.setInvestmentPlatformType(InvestmentPlatform.TIANTIAN_FUND);
+        nfttl.setName("南方天天利货币B");
+        nfttl.setPrinciple(Amount.integer(2000));
+        nfttl.setAnnualYield(Percent.valueOf(4.7290));
+        nfttl.setBuyDate(new LocalDate("2018-02-21"));
+        nfttl.setValueDate(new LocalDate("2018-02-23"));
+        mInvestmentProjects.add(nfttl);
     }
 
     public static InvestmentProjectLab getInstance() {
