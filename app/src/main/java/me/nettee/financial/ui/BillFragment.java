2e7b53333130035d3dc1f8f9860ed8b8b3c35506
William <nettee.liu@gmail.com>
2018-03-12 20:22:55 +0800

    some in bill fragment

--- app/src/main/java/me/nettee/financial/ui/BillFragment.java
+++ app/src/main/java/me/nettee/financial/ui/BillFragment.java
@@ -5,10 +5,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.joda.time.LocalDate;
+
+import java.util.Locale;
 
 import me.nettee.financial.R;
 
 public class BillFragment extends Fragment {
+
+    private LinearLayout mBillListList;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
@@ -17,6 +25,28 @@ public class BillFragment extends Fragment {
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
+
+        mBillListList = view.findViewById(R.id.bill_list_list);
+
+        mBillListList.removeAllViews();
+
+        LayoutInflater inflater = LayoutInflater.from(getActivity());
+
+        LocalDate now = LocalDate.now();
+        int year = now.getYear();
+        int month = now.getMonthOfYear();
+
+        for (int m = month; m > 0; m--) {
+
+            View billList = inflater.inflate(R.layout.title_bar_bill_list, null);
+            int itemHeight = (int) getResources().getDimension(R.dimen.height_title_bar_category);
+            billList.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, itemHeight));
+
+            TextView billListTitle = billList.findViewById(R.id.bill_list_title);
+            billListTitle.setText(String.format(Locale.CHINA, "%d年%d月", year, m));
+
+            mBillListList.addView(billList);
+        }
     }
 
 }
