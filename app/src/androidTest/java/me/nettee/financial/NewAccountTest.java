ba6cfa3a4edc79a2350e3e654d18043ff7737254
William <nettee.liu@gmail.com>
2018-05-18 19:43:17 +0800

    finish new account test cases

--- app/src/androidTest/java/me/nettee/financial/NewAccountTest.java
+++ app/src/androidTest/java/me/nettee/financial/NewAccountTest.java
@@ -81,16 +81,14 @@ public class NewAccountTest {
 
         @Override
         protected InputOperation[] getInputOperations() {
-            return new InputOperation[] {
-                    () -> onView(allOf(withId(R.id.input_bar_text_content), withParent(withId(R.id.account_remark))))
-                            .check(matches(isDisplayed()))
-                            .perform(replaceText("钱包B")),
-                    () -> onView(allOf(withId(R.id.input_bar_amount_content), withParent(withId(R.id.account_balance))))
-                            .check(matches(isDisplayed()))
-                            .perform(typeText("1234.00")),
-            };
+            InputOperation remark = () -> onView(allOf(withId(R.id.input_bar_text_content), withParent(withId(R.id.account_remark))))
+                    .check(matches(isDisplayed()))
+                    .perform(replaceText("钱包B"));
+            InputOperation balance = () -> onView(allOf(withId(R.id.input_bar_amount_content), withParent(withId(R.id.account_balance))))
+                    .check(matches(isDisplayed()))
+                    .perform(typeText("1234.00"));
+            return new InputOperation[] {remark, balance};
         }
-
     }
 
     private static class CreditCardAccountCreator extends AccountCreator {
@@ -102,20 +100,19 @@ public class NewAccountTest {
 
         @Override
         protected InputOperation[] getInputOperations() {
-            return new InputOperation[] {
-                    () -> onView(allOf(withId(R.id.input_bar_text_content), withParent(withId(R.id.account_remark))))
-                            .check(matches(isDisplayed()))
-                            .perform(replaceText("刷刷卡")),
-                    () -> onView(allOf(withId(R.id.input_bar_bank_card_number_content), withParent(withId(R.id.account_bank_card_number))))
-                            .check(matches(isDisplayed()))
-                            .perform(typeText("994852")),
-                    () -> onView(allOf(withId(R.id.input_bar_amount_content), withParent(withId(R.id.account_credit_limit))))
-                            .check(matches(isDisplayed()))
-                            .perform(typeText("5000.00")),
-                    () -> onView(allOf(withId(R.id.input_bar_amount_content), withParent(withId(R.id.account_arrears))))
-                            .check(matches(isDisplayed()))
-                            .perform(typeText("4239.32")),
-            };
+            InputOperation remark = () -> onView(allOf(withId(R.id.input_bar_text_content), withParent(withId(R.id.account_remark))))
+                    .check(matches(isDisplayed()))
+                    .perform(replaceText("刷刷卡"));
+            InputOperation bankCardNumber = () -> onView(allOf(withId(R.id.input_bar_bank_card_number_content), withParent(withId(R.id.account_bank_card_number))))
+                    .check(matches(isDisplayed()))
+                    .perform(typeText("994852"));
+            InputOperation creditLimit = () -> onView(allOf(withId(R.id.input_bar_amount_content), withParent(withId(R.id.account_credit_limit))))
+                    .check(matches(isDisplayed()))
+                    .perform(typeText("5000.00"));
+            InputOperation arrears = () -> onView(allOf(withId(R.id.input_bar_amount_content), withParent(withId(R.id.account_arrears))))
+                    .check(matches(isDisplayed()))
+                    .perform(typeText("4239.32"));
+            return new InputOperation[] {remark, bankCardNumber, creditLimit, arrears};
         }
 
     }
@@ -129,19 +126,96 @@ public class NewAccountTest {
 
         @Override
         protected InputOperation[] getInputOperations() {
-            return new InputOperation[] {
-                    () -> onView(allOf(withId(R.id.input_bar_text_content), withParent(withId(R.id.account_remark))))
-                            .check(matches(isDisplayed()))
-                            .perform(replaceText("存存卡")),
-                    () -> onView(allOf(withId(R.id.input_bar_bank_card_number_content), withParent(withId(R.id.account_bank_card_number))))
-                            .check(matches(isDisplayed()))
-                            .perform(typeText("852340")),
-                    () -> onView(allOf(withId(R.id.input_bar_amount_content), withParent(withId(R.id.account_balance))))
-                            .check(matches(isDisplayed()))
-                            .perform(typeText("50293.04")),
-            };
+            InputOperation remark = () -> onView(allOf(withId(R.id.input_bar_text_content), withParent(withId(R.id.account_remark))))
+                    .check(matches(isDisplayed()))
+                    .perform(replaceText("存存卡"));
+            InputOperation bankCardNumber = () -> onView(allOf(withId(R.id.input_bar_bank_card_number_content), withParent(withId(R.id.account_bank_card_number))))
+                    .check(matches(isDisplayed()))
+                    .perform(typeText("852340"));
+            InputOperation balance = () -> onView(allOf(withId(R.id.input_bar_amount_content), withParent(withId(R.id.account_balance))))
+                    .check(matches(isDisplayed()))
+                    .perform(typeText("50293.04"));
+            return new InputOperation[] {remark, bankCardNumber, balance};
+        }
+    }
+
+    private static class AlipayAccountCreator extends AccountCreator {
+
+        @Override
+        protected int getCandidateViewText() {
+            return R.string.candidate_account_name_alipay;
         }
 
+        @Override
+        protected InputOperation[] getInputOperations() {
+            InputOperation remark = () -> onView(allOf(withId(R.id.input_bar_text_content), withParent(withId(R.id.account_remark))))
+                    .check(matches(isDisplayed()))
+                    .perform(replaceText("支付宝宝"));
+            InputOperation balance = () -> onView(allOf(withId(R.id.input_bar_amount_content), withParent(withId(R.id.account_balance))))
+                    .check(matches(isDisplayed()))
+                    .perform(typeText("4685.30"));
+            InputOperation yuebao = () -> onView(allOf(withId(R.id.input_bar_toggle_switch), withParent(withId(R.id.account_yuebao_open))))
+                    .check(matches(isDisplayed()))
+                    .perform(click());
+            return new InputOperation[] {remark, balance, yuebao};
+        }
+    }
+
+    private static class WeixinAccountCreator extends AccountCreator {
+
+        @Override
+        protected int getCandidateViewText() {
+            return R.string.candidate_account_name_weixin;
+        }
+
+        @Override
+        protected InputOperation[] getInputOperations() {
+            InputOperation remark = () -> onView(allOf(withId(R.id.input_bar_text_content), withParent(withId(R.id.account_remark))))
+                    .check(matches(isDisplayed()))
+                    .perform(replaceText("微信钱包包"));
+            InputOperation balance = () -> onView(allOf(withId(R.id.input_bar_amount_content), withParent(withId(R.id.account_balance))))
+                    .check(matches(isDisplayed()))
+                    .perform(typeText("45.00"));
+            return new InputOperation[] {remark, balance};
+        }
+    }
+
+    private static class CampusCardAccountCreator extends AccountCreator {
+
+        @Override
+        protected int getCandidateViewText() {
+            return R.string.candidate_account_name_campus_card;
+        }
+
+        @Override
+        protected InputOperation[] getInputOperations() {
+            InputOperation remark = () -> onView(allOf(withId(R.id.input_bar_text_content), withParent(withId(R.id.account_remark))))
+                    .check(matches(isDisplayed()))
+                    .perform(replaceText("丑丑卡"));
+            InputOperation balance = () -> onView(allOf(withId(R.id.input_bar_amount_content), withParent(withId(R.id.account_balance))))
+                    .check(matches(isDisplayed()))
+                    .perform(typeText("120.01"));
+            return new InputOperation[] {remark, balance};
+        }
+    }
+
+    private static class BusCardAccountCreator extends AccountCreator {
+
+        @Override
+        protected int getCandidateViewText() {
+            return R.string.candidate_account_name_bus_card;
+        }
+
+        @Override
+        protected InputOperation[] getInputOperations() {
+            InputOperation remark = () -> onView(allOf(withId(R.id.input_bar_text_content), withParent(withId(R.id.account_remark))))
+                    .check(matches(isDisplayed()))
+                    .perform(replaceText("Hello kitty"));
+            InputOperation balance = () -> onView(allOf(withId(R.id.input_bar_amount_content), withParent(withId(R.id.account_balance))))
+                    .check(matches(isDisplayed()))
+                    .perform(typeText("82.56"));
+            return new InputOperation[] {remark, balance};
+        }
     }
 
     @Test
@@ -151,6 +225,10 @@ public class NewAccountTest {
                 new CashAccountCreator(),
                 new CreditCardAccountCreator(),
                 new DebitCardAccountCreator(),
+                new AlipayAccountCreator(),
+                new WeixinAccountCreator(),
+                new CampusCardAccountCreator(),
+                new BusCardAccountCreator(),
         };
 
         for (AccountCreator creator : creators) {
